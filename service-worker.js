3f5bc9dc4d9110c5df7f5cbb601b45deb3bf7b4a
Juscelino de Moraes Gonçalves Junior <juscelinomgjunior@gmail.com>
2024-11-09 15:23:12 -0500

    cache only strategy

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,30 @@ self.addEventListener('install', (event) => {
 console.log ('[SW] Install:', event);
 
 self.skipWaiting();
+
+console.log(caches);
+event.waitUntil(
+
+    caches.open('cache-Assets')
+    .then((cache) =>{ 
+    console.log('cache:', cache);
+    cache.add('/index.html');
+    cache.add('/script.js');
+    cache.add('/styles.css');
+    cache.add("/manifest.json")
+    cache.add('/icons/favicon.ico')
+    cache.add('/icons/favicon.svg')
+    cache.add('/icons/web-app-manifest-192x192.png')
+    })
+    .catch((error) =>{ 
+    
+        console.log('cache Failed:', error);
+        
+    })
+ 
+);
+
+
 });
 
 
@@ -14,6 +38,19 @@ event.waitUntil(clients.claim());
 });
 
 
-self.addEventListener('fetch', () => {
-    return;
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) =>{
+console.log('Response:', response);
+return response;
+    })
+    .catch((error) =>{
+        console.log('Match Failed', error );
+    }
+    )
+
+  );
+  
+
 });
